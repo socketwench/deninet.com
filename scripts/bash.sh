18672673f63d71b2ed7b4cf167e611c323db3bb5
socketwench <tess@deninet.com>
2015-12-26 22:33:40 -0600

    Imported scripts from Dropwhale.

---
+++ scripts/bash.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+SCRIPT_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+
+DIR=$(dirname $SCRIPT_DIR)
+
+PS=$(cd $DIR && docker-compose ps -q)
+
+if [[ $PS == '' ]]; then
+	echo "No containers found."
+	exit
+fi
+
+if [[ $1 == '' ]]; then
+	echo "Starting bash..."
+	docker exec -it $(cd $DIR && docker-compose ps -q cli) bash -i
+else
+	docker exec -it $(cd $DIR && docker-compose ps -q cli) bash -ic "$*"
+fi
--- scripts/build.sh
+++ scripts/build.sh
@@ -2,4 +2,4 @@
 
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
-${DIR}/docker-bash /root/scripts/rebuild.sh
+${DIR}/bash.sh /root/scripts/rebuild.sh
