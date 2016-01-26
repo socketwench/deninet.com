e398ed06c70d02deeac14a1bb58040fad2d72e5a
socketwench <tess@deninet.com>
2016-01-25 21:44:31 -0600

    Generated web_dev feature module.

--- .docker/cli/demo.sh
+++ .docker/cli/demo.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
 
 drush si --uri=http://web.dev --root=/var/www --db-url=mysql://${DB_1_ENV_MYSQL_USER}:${DB_ENV_MYSQL_PASS}@db/${DB_1_ENV_MYSQL_DB} -y deninet --sites-subdir=web.dev
+
+drush en --uri=http://web.dev --root=/var/www -y web_dev
