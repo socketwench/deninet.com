9e795ccf482758310e51ac922aa8fb24ba48fd99
socketwench <tess@deninet.com>
2016-01-27 20:46:49 -0600

    Renamed web_dev module to deninet_dev.

--- .docker/cli/demo.sh
+++ .docker/cli/demo.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-drush si --uri=http://web.dev --root=/var/www --db-url=mysql://${DB_1_ENV_MYSQL_USER}:${DB_ENV_MYSQL_PASS}@db/${DB_1_ENV_MYSQL_DB} -y deninet --sites-subdir=web.dev
+drush si --uri=http://deninet.dev --root=/var/www --db-url=mysql://${DB_1_ENV_MYSQL_USER}:${DB_ENV_MYSQL_PASS}@db/${DB_1_ENV_MYSQL_DB} -y deninet --sites-subdir=deninet.dev
 
-drush en --uri=http://web.dev --root=/var/www -y web_dev
+drush en --uri=http://deninet.dev --root=/var/www -y deninet_dev
